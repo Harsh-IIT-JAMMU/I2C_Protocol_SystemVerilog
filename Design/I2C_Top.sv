fc951e72fdf6151ddae61a775a6acfabe9e14256
Harsh-IIT-JAMMU <2025psp0003@iitjammu.ac.in>
2026-05-10 13:21:44 +0530

    Update I2C_Top.sv

--- Design/I2C_Top.sv
+++ Design/I2C_Top.sv
@@ -19,18 +19,3 @@ assign ack_err = ack_errs | ack_errm;
  
  
 endmodule
- 
-///////////////////////////////////////////////////////
-interface i2c_if;
-  
-  logic clk;
-  logic rst;
-  logic newd;
-  logic op;   
-  logic [7:0] din;
-  logic [6:0] addr;
-  logic [7:0] dout;
-  logic  done;
-  logic busy, ack_err;  
-  
-endinterface
